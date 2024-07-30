5136efa5cb9a6b458a7b36c7ed517bd90fadd9f2
DoTRung1023 <a1899443@adelaide.edu.au>
2024-07-31 02:35:08 +0930

    fix 1-3

--- function-1-3.cpp
+++ function-1-3.cpp
@@ -6,7 +6,7 @@ void count_digits(int array[4][4]) {
     int count[10] = {0};
     for (int i = 0; i < 4; i++) {
         for (int j = 0; j < 4; j++) {
-            count[array[i][j]-1]++;
+            count[array[i][j]]++;
         }
     }
     for (int i = 0; i < 9; i++) {
