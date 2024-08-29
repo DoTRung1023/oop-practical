68020e1adc1b584ed6c99404cf2835e147a7300d
DoTRung1023 <a1899443@adelaide.edu.au>
2024-08-29 12:21:56 +0930

    unpark

--- main-1-2.cpp
+++ main-1-2.cpp
@@ -16,33 +16,31 @@ int main(){
     ParkingLot arrayParkinglot(parkNum);
 
     for(int i = 0; i<parkNum; i++) {
-        int typeVehicle;
+        string typeVehicle;
         int inputID;
 
-        cout << "Enter type of vehicle(1-car   2-bus   3-motorbike): ";
+        cout << "Enter type of vehicle: ";
         cin >> typeVehicle;
 
         cout << "Enter the vehicle ID: ";
         cin >> inputID;
 
-        switch(typeVehicle) {
-            case 1:
-                Vehicle* parkVehicle = new Car(inputID);
-                parkVehicle->set_ID(inputID);
-                arrayParkinglot.parkVehicle(parkVehicle);
-                break;
-            case 2:
-                Vehicle* parkVehicle = new Bus(inputID);
-                parkVehicle->set_ID(inputID);
-                arrayParkinglot.parkVehicle(parkVehicle);
-                break;
-            case 3:
-                Vehicle* parkVehicle = new Motorbike(inputID);
-                parkVehicle->set_ID(inputID);
-                arrayParkinglot.parkVehicle(parkVehicle);
-                break;
-                }
+        if(typeVehicle == "car") {
+            Car* c = new Car(inputID);
+            c->set_ID(inputID);
+            arrayParkinglot.parkVehicle(c);
         }
+        else if(typeVehicle == "bus") {
+            Bus* b = new Bus(inputID);
+            b->set_ID(inputID);
+            arrayParkinglot.parkVehicle(b);
+        }
+        else if(typeVehicle == "motorbike") {
+            Motorbike* m = new Motorbike(inputID);
+            m->set_ID(inputID);
+            arrayParkinglot.parkVehicle(m);
+        }
+    }
     int unparkID;
     cout << "Enter ID of a vehicle to unpark: ";
     cin >> unparkID;
